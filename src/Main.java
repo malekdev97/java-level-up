803f97e6c635ca087b29dd1f101077761dd5ca54
Abdulmalik <aomayri970@gmail.com>
2022-04-26 23:43:31 +0300

    Add BST

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,11 @@
 import Tree.*;
+import Search.Search;
 
 public class Main {
 
   public static void main(String[] args) {
 
-    BinaryTree tree = new BinaryTree();
-    tree.insert(30);
-    tree.insert(20);
-    tree.insert(25);
-    tree.insert(40);
-    tree.insert(35);
-    tree.insert(50);
-
-    BinaryTree.postorder(tree.root);
-
-
+    System.out.println(Search.search(20));
   }
+
 }
--- src/Search/Search.java
+++ src/Search/Search.java
@@ -6,6 +6,25 @@ public class Search {
 
     // Binary Search Tree
     // code
+    public static int search(int item)
+    {
+        int[] arr = {10, 20, 30, 40, 50, 60, 70, 80};
+        int middle = 0, low = 0, high = arr.length-1;
+        while(low <= high)
+        {
+            middle = low + high / 2;
+            if (item == arr[middle]) {
+                return middle;
+            }
+            else if (item < arr[middle]) {
+                high = middle - 1;
+            }
+            else if (item > arr[middle]) {
+                low = middle + 1;
+            }
+        }
+        return -1;
+    }
 
     // Interpolation Search
     // code
