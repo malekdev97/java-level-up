96b03106aa2b1bdcdef74a16c105eed29cc31a7d
Abdulmalik <abdulmalik-abdullah@hotmail.com>
2022-05-02 17:01:53 +0300

    add email extraction class

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
-import Tree.*;
-import Search.Search;
+import java.io.*;
+import java.util.regex.Pattern;
+
 
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
 
-    System.out.println(Search.search(20));
+    // will print the extracted emails
+    PrintWriter pw = new PrintWriter("output.txt");
+
+    // I don't know what the hell is this validation
+    // ,I only know that it will extract the emails
+    Pattern p = Pattern.compile("[a-zA-Z0-9]"
+            + "[a-zA-Z0-9_.]"
+            + "*@[a-zA-Z0-9]"
+            + "+([.][a-zA-Z]+)+");
+    // the txt file which we will read and extract the email from
+    BufferedReader br = new BufferedReader( new FileReader("input.txt"));
+
+    EmailExtraction.extractEmail(br, pw, p);
   }
 
 }
