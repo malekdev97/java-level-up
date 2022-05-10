262e568326a9bce037fd780155f2606fc63708ba
Abdulmalik <abdulmalik-abdullah@hotmail.com>
2022-05-10 22:38:04 +0300

    add sort

---
+++ src/sorting/Sort.java
@@ -0,0 +1,47 @@
+package sorting;
+
+public class Sort {
+    static void bubbleSort(int array[]) {
+        int size = array.length;
+
+        // loop to access each array element
+        for (int i = 0; i < size - 1; i++)
+
+            // loop to compare array elements
+            for (int j = 0; j < size - i - 1; j++)
+
+                // compare two adjacent elements
+                // change > to < to sort in descending order
+                if (array[j] > array[j + 1]) {
+
+                    // swapping occurs if elements
+                    // are not in the intended order
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+    }
+
+    void selectionSort(int array[]) {
+        int size = array.length;
+
+        for (int step = 0; step < size - 1; step++) {
+            int min_idx = step;
+
+            for (int i = step + 1; i < size; i++) {
+
+                // To sort in descending order, change > to < in this line.
+                // Select the minimum element in each loop.
+                if (array[i] < array[min_idx]) {
+                    min_idx = i;
+                }
+            }
+
+            // put min at the correct position
+            int temp = array[step];
+            array[step] = array[min_idx];
+            array[min_idx] = temp;
+        }
+
+    }
+}
