db7cc88bf9a577de271485af94fe7a00599ace1f
Abdulmalik <abdulmalik-abdullah@hotmail.com>
2022-05-17 02:03:05 +0300

    add queue implementation

--- src/List/Queue.java
+++ src/List/Queue.java
@@ -1,7 +1,87 @@
 package List;
 
 public class Queue {
-    public Node head;
-    public Node tail;
+
+    public int size = 5;
+
+    public int [] items = new int[size];
+    public int rear;
+    public int front;
+
+    public Queue() {
+        this.rear = -1;
+        this.front = -1;
+    }
+
+    boolean isFull() {
+        if(front == 0 && rear == size -1) {
+            return true;
+        }
+        return false;
+    }
+
+    boolean isEmpty() {
+        if(front == -1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+    public void enqueue(int item) {
+        if(isFull()) {
+            System.out.println("Queue is full");
+        }
+
+        else {
+            if(isEmpty()){
+                front = 0;
+            }
+
+            rear++;
+            items[rear] = item;
+            System.out.println("Inserted: "+ item);
+        }
+    }
+
+    public int dequeue() {
+        int item ;
+        if(isEmpty()) {
+            System.out.println("Queue is Empty");
+            return -1;
+        }
+        else {
+            item = items[front];
+            if(front >= rear) {
+                front = -1;
+                rear = -1;
+            }
+            else {
+                front++;
+            }
+            System.out.println("Dequeue: "+ item);
+            return(item);
+        }
+    }
+
+    public void display() {
+        int i;
+        if(isEmpty()) {
+            System.out.println("Empty Queue");
+        }
+        else {
+            System.out.println("\nFront index: "+ front);
+            System.out.println("Items: ");
+
+            for (i = front; i<= rear; i++) {
+                System.out.println(items[i] + " ");
+            }
+
+            System.out.println("\n Rear index: "+ rear);
+        }
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 import Tree.BinaryTree;
 import Tree.BinaryTree;
+import List.Queue;
+import List.Node;
 
 import static Tree.BinaryTree.inOrder;
 
@@ -14,18 +16,15 @@ public class Main {
   public static void main(String[] args)
           throws IOException {
 
-    BinaryTree tree=new BinaryTree();
-    // inserting node one by one in Binary Search Tree.Tree
-    tree.insert(30);
-    tree.insert(50);
-    tree.insert(45);
-    tree.insert(55);
-    tree.insert(20);
-    tree.insert(15);
-    tree.insert(25);
+    Queue queue = new Queue();
 
-     inOrder(tree.root);
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
+    queue.enqueue(4);
+    queue.enqueue(5);
+
+    queue.display();
   }
-
 
 }
