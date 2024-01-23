cc87acb678976b33b15967638c665ae9f29126e0
Malek <omayri970@gmail.com>
2024-01-23 23:13:01 +0300

    update folders

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,3 @@
-// Authors: Abdulmalik Abdullah
-// Date: 01/01/2024
-
-import List.Deque;
 
 import java.util.Random;
 
@@ -10,20 +6,6 @@ public class Main {
   public static void main(String[] args) {
     // Your code here
 
-    Random random = new Random();
-
-    Deque deque = new Deque();
-
-    deque.addLast(20);
-    deque.addFirst(10);
-    deque.addFirst(5);
-    deque.addLast(30);
-
-    deque.removeFirst();
-    deque.removeLast();
-
-    System.out.println(deque.front.data);
-    System.out.println(deque.rear.data);
 
   }
 
--- src/OOP/Subject.java
+++
@@ -1,9 +0,0 @@
-package OOP;
-
-// Final class
-public final class Subject {
-
-    final String desc = "No one can change me\nI'm Final!";
-
-
-}
