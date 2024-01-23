023b51323190e69cb516dd2e5b930b0158fd7bd7
Malek <omayri970@gmail.com>
2024-01-23 23:09:57 +0300

    add deque

--- src/List/Node.java
+++ src/List/Node.java
@@ -5,6 +5,15 @@ public class Node {
     public int data;
     public Node next;
     public Node previous;
-    
+
+    public Node(){
+
+    }
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.previous = null;
+    }
 }
 
