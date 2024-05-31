c6ffd9c7b79b9f0023483483a5f138bc84821051
Malek <omayri970@gmail.com>
2024-05-31 21:11:31 +0300

    add comment

--- src/exam/Main.java
+++ src/exam/Main.java
@@ -4,22 +4,66 @@ public class Main {
     
 
     public static void main(String [] args) {
-        
-        // Equal and ==
-        String str1 = new String("one");
-        String str2 = new String("two");
-        String str3 = "two";
+    // Java SE Programmer I 1Z0-808
+    //     - Basic 
+	// 	○ Scope variables
+	// 	○ Class structure
+	// 	○ Run java via CMD
+	// 	○ Compare component oo, platform independence, etc.
+	// 	○ Import java packages 
+		
+	// - Working With Java Data Types
+	// 	○ All Types + Casting 
+	// 	○ Object reference variables vs primitive variables
+	// 	○ Read object field
+	// 	○ Object lifecycle (creation, dereference, reassignment and garbage collection
+	// 	○ Wrapper classes Boolean, Double, Integer
+		
+	// - Using Operators and Decision Constructs
+	// 	○ Java operators and parentheses
+	// 	○ == and equals()
+	// 	○ If, if else, and else statement
+	// 	○ Switch statement
+	// - Creating and Using Arrays
+	// 	○ One dimensional array
+	// 	○ Multi dimensional array
+	// - Using Loop Constructs
+	// 	○ while loops
+	// 	○ Enhanced for loop
+	// 	○ Do while
+	// 	○ Break and Continue
+	// 	○ Compare loop constructs
+	// 	○  
+	// - Working with Methods and Encapsulation
+	// 	○ Access modifiers
+	// 	○ Encapsulation
+	// 	○ Method with return value + overloaded
+	// 	○ Static methods and attributes
+	// 	○ Default and user defined + overload constructors 
+		
+	
+	// - Working with Inheritance
+	// 	○ Describe inheritance and its benefits 
+	// 	○ When casting is necessary
+	// 	○ Polymorphism + overrides + type reference and object
+	// 	○ Super keword 
+	// 	○ Abstract class 
+	// 	○ Interface 
+	// - Handling Exceptions
+	// 	○ Checked exceptions, unchecked exception, and errors
+	// 	○ Method that throws an exception
+	// 	○ Try-catch
+	// 	○ Common exception classes(NullPointerExc, ArrayIndexOutOfBounds)
+	// 	○ Advantages of exception handling
+		
+	// - Working with Selected classes from the Java API
+	// 	○ StringBuilder class and its methods
+	// 	○ ArrayList
+	// 	○ Manipulate String
+	// 	○ Manipulate calendar data from java.time.LocalDateTime, formatter, period 
+	// 	○ Lambda expression and predicate expression
 
-        System.out.println(str2.equals(str3));
-        System.out.println(str1);
-        System.out.println(1 == 1.0);
-        
-        // _ _ _ _ _ _ _ _ _ //
 
-
-        // primitive data type and none-primitive data type
-
-        
 
     }
 }
