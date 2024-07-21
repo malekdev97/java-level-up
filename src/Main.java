29e238617fe4786dee03686f2a2db0716a0a4626
malekcoders <149315149+malekcoders@users.noreply.github.com>
2024-07-21 15:12:30 +0300

    Recursion edit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 class Main {
-  public static void main(String []args) {
-    
-  }
+  public static void main(String[] args) {
+    int n = 6;
+    // 0 + 1 = 1
+    // 1 + 1 = 2
+    // 2 + 3 = 5
+    // 3 + 5 = 8
+    System.out.println(fib(n));
+}
+
+  public static int fib(int n) {
+      if (n <= 1) {
+          return n;
+      }
+      return fib(n - 1) + fib(n - 2);
+    }
+  
+  
 }
