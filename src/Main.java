f4d8b3a68f6ede1544be62a35af9df7f46f3acf8
Abdulmalik <omayri970@gmail.com>
2024-12-02 17:55:03 +0300

    add read/write file using InputOutputStream

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,6 @@ public class Main {
     
     public static void main(String[] args) {
         
-
-
-       byte[][] v = {{4, 2, 8, 5},
-                    {9, 3, 8, 4},
-                    {5, 6, 7, 8},
-                    {8, 4, 5, 6}};
-
-        byte r = 0;
-
-        for (byte i = 0; i < v.length; i++) {
-            for (byte j = 0; j < v[i].length; j++) {
-                System.out.println(v[i][j]);
-            }
-        }
+        // 
     }
 }
---
+++ src/readfile/Main.java
@@ -0,0 +1,37 @@
+package readfile;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.LinkedList;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+
+
+        String fileName = "example.txt";
+        String content = "Java is the best language";
+        // Easy beasy lemon squzzy
+        createFile(fileName, content);
+        outputFile(fileName);
+    }
+
+    public static void createFile(String fileName, String fileContent) throws IOException {
+
+        // [Input_String] -> [bytes] -> [file] -> [bytes] -> [Ouput_String] 
+
+        FileOutputStream fos = new FileOutputStream(fileName, true);
+        fos.write(fileContent.getBytes()); 
+        fos.close();
+    }
+
+    public static void outputFile(String filePath) throws IOException {
+        int counter = 0;
+        for (String line : Files.readAllLines(Path.of("example.txt"))) {
+            System.out.print(line);
+            System.out.println(" "+ ++counter);
+        }
+    }
+}
