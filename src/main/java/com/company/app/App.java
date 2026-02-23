cbc26a346162f17671c36fa8515819467fa1196f
Abdulmalik <abdulmalik-abdullah@hotmail.com>
2026-02-23 23:46:51 +0300

    add leetcode exmaple 1

--- src/main/java/com/company/app/App.java
+++ src/main/java/com/company/app/App.java
@@ -7,34 +7,17 @@ import static java.time.chrono.JapaneseEra.values;
 public class App {
     public static void main(String[] args) {
 
-        // Enum Lesson one
-        DaysOfWeek sunday = DaysOfWeek.SUNDAY;
 
-        if(sunday == DaysOfWeek.SUNDAY) {
-            System.out.println("Sunday!");
-        }
+        String name = "joN";
+        System.out.println(isLowerCase(name));
+    }
 
-        System.out.println(DaysOfWeek.valueOf("FRIDAY"));
-        System.out.println(DaysOfWeek.SUNDAY.ordinal());
-        System.out.println(DaysOfWeek.MONDAY.name());
-        System.out.println(Arrays.toString(DaysOfWeek.values()) );
-        if(DaysOfWeek.values() instanceof Object[]) {
-            System.out.println(Boolean.valueOf(true)); // ✅ true
-        }
-        if(DaysOfWeek.values() instanceof DaysOfWeek[])
-            System.out.println(); // ✅ true
-
-        // if (DaysOfWeek.values() instanceof String[]) { // compiler error type mismatch
-           //  System.out.println(true); // ❌ false
-
-
-        // if(DaysOfWeek.values() instanceof java.util.List) // compiler error type mismatch
-            // System.out.println(true); // ❌ false
-
-        for(DaysOfWeek day : DaysOfWeek.values()) {
-            if(day == DaysOfWeek.FRIDAY || day == DaysOfWeek.SATURDAY)
-                System.out.println("Weekend");
+    public static boolean isLowerCase(String str) {
+        for(char c : str.toCharArray()) {
+            if(!Character.isLowerCase(c)) {
+                return false;
             }
         }
-
+        return true;
+    }
 }
